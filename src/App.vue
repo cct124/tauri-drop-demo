1371a53939d2a0e89ee31f35c14c21911718ac01
huangdonghao <huangdonghao@miya818.com>
2023-05-31 18:36:31 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
-  <div></div>
+  <div class="app"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as event from "@tauri-apps/api/event";
 
-<style scoped></style>
+event.listen("tauri://file-drop", (e) => {
+  console.log("Tauri event", e);
+});
+
+event.listen("tauri://file-drop-hover", (e) => {
+  console.log("Tauri event", e);
+});
+
+event.listen("tauri://file-drop-cancelled", (e) => {
+  console.log("Tauri event", e);
+});
+
+// // 处理拖放区域的dragover事件
+// function handleDragOver(event: DragEvent) {
+//   event.stopPropagation();
+//   event.preventDefault();
+// }
+
+// // 处理拖放区域的drop事件
+// function handleFileSelect(event: DragEvent) {
+//   event.stopPropagation();
+//   event.preventDefault();
+//   if (event.dataTransfer) {
+//     for (const file of event.dataTransfer.files) {
+//       console.log("Browser event", file.name, file.size, file.type);
+//     }
+//   }
+// }
+</script>
+
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  background-color: #efefef;
+}
+</style>
